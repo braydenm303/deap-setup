3b58eb3980ecb676ffacd62821415f80b8e5360e
Brayden Morris <braydenm303@gmail.com>
2017-09-05 15:23:02 -0600

    refactor scripts

---
+++ bin/bootstrap.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# first check for internet connectivity.
+# if no connection, configure wifi.
+test=github.com
+if nc -zw1 $test 443 && echo |openssl s_client -connect $test:443 2>&1 |awk '
+	handshake && $1 == "Verification" { if ($2=="OK") exit; exit 1 }
+	$1 $2 == "SSLhandshake" { handshake = 1 }'
+then
+	echo "Internet connection verified."
+else
+	echo "No internet connection."
+	echo "Scanning for available networks..."
+	sudo iwlist wlan0 scan | grep SSID
+	read -p "Wifi name (SSID): " wifi_name
+	read -p "Wifi password (PSK): " wifi_password
+	wpa_passphrase "$wifi_name" "$wifi_password" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null
+	sudo wpa_cli reconfigure
+fi
--- bin/setup.sh
+++ bin/setup.sh
@@ -15,30 +15,31 @@
 sudo apt-get update
 
 # apply any software updates
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # apply any system updates
-sudo apt-get dist-upgrade
+sudo apt-get dist-upgrade -y
 
 # clean up after ourselves
 sudo apt-get clean
 
 # install the graphic interfaces
-sudo apt-get install --no-install-recommends xserver-xorg xinit raspberrypi-ui-mods gvfs
+sudo apt-get install -y xserver-xorg xinit raspberrypi-ui-mods gvfs
 
 # install applications
-sudo apt-get install lxterminal qjackctl zynaddsubfx
+sudo apt-get install -y lxterminal qjackctl zynaddsubfx
 # TODO: check if enabling Jack realtime system priority is what is causing deadlock
 
 # install pure data
 # download package
-wget "https://github.com/agraef/purr-data/releases/download/2.2.3/pd-l2ork-2.2.3-raspbian-armv7l.deb" -O /tmp/pd-l2ork.deb
+PD_VERSION="2.2.3"
+wget 'https://github.com/agraef/purr-data/releases/download/'"$PD_VERSION"'/pd-l2ork-'"$PD_VERSION"'-raspbian-armv7l.deb' -O /tmp/pd-l2ork.deb
 # install package
 sudo dpkg -i /tmp/pd-l2ork.deb
 # fix unmet dependencies
 sudo apt-get -f install
 # install other dependencies
-sudo apt-get install libnss3 libgconf2-4
+sudo apt-get install -y libnss3 libgconf2-4
 
 # reboot into graphic interface
 sudo reboot
